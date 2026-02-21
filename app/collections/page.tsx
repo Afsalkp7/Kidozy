f0750dc7181b154a5e132e9cd8f5cc6fddb6ace3
Afsalkp7 <afsalkpmanu31@gmail.com>
2026-02-22 01:18:51 +0530

    about section completed

--- app/collections/page.tsx
+++ app/collections/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import StyleCatalog from "../components/collections/CollectionCatelogue";
+import HeroSectionCatelogue from "../components/collections/HeroSection";
 
 const page = () => {
   return (
     <div className="main-padding">
+      <HeroSectionCatelogue />
       <StyleCatalog />
     </div>
   );
---
+++ app/components/collections/HeroSection.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HeroSectionCatelogue = () => {
+  return (
+    <div className="flex flex-col justify-center text-center my-3 sm:my-5 md:my-10 ">
+      <h1 className="text-[18px] sm:text-[25px] md:text-[30p x] lg:text-[40px] font-extrabold">
+        Our Latest Looks
+      </h1>
+      <p className="text-xs sm:text-sm md:text-md lg:text-lg text-slate-500">
+        Follow our style journey. Click any look to inquire on Instagram.
+      </p>
+    </div>
+  );
+};
+
+export default HeroSectionCatelogue;
